18f2c407a912cbc89394c25f9ffde383a99ba50e
jlb64 <jliogerbun+github@gmail.com>
2023-03-07 10:06:29 +0100

    je commence le js pour augmenter le nombre d'achat dans songs

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*document.addEventListener('click',function(event){
+    var id =event.target.id;
+    var className=document.getElementById(id).getAttribute("class");
+ alert(id);
+ alert(className);
+    });*/
+
+document.addEventListener('click', function(event) {
+    
+})
