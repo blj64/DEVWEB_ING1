bc4a924c1055d2f1552c16d405d0501fa78ca194
jlb64 <jliogerbun+github@gmail.com>
2023-03-09 12:11:34 +0100

    js pour ajouter une quantite aux produits et afficher son stock

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-const elt = document.getElementsByClassName("quantity-more");
+const more = document.getElementsByClassName("quantity-more");
+const less = document.getElementsByClassName("quantity-less");
+const stockBtn = document.getElementsByClassName("show-stock")
 
-for (let i = 0; i < elt.length; i++) {
-    elt[i].addEventListener("click", myFunction);
+for (let i = 0; i < more.length; i++) {
+    more[i].addEventListener("click", () => {
+        document.getElementById("quantity-number-"+more[i].dataset.song).value++
+
+    });
+    less[i].addEventListener("click", () => {
+        const value = document.getElementById("quantity-number-"+less[i].dataset.song)
+        if (value.value > 0) {
+            value.value--
+        }
+
+    });
+
+    stockBtn[i].addEventListener("click", () => {
+        const stockValue = document.getElementById("stock-number-"+ stockBtn[i].dataset.song)
+        stockValue.style.display =  stockValue.style.display == 'block' ? 'none' : 'block' 
+    })
+
+   
     
 }
 
-function myFunction(){
-    alert(this.id);
-}
