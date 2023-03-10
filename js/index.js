d323d2bc32200ce4e89dee9fbc5b1a4d8b719f6e
jlb64 <jliogerbun+github@gmail.com>
2023-03-10 12:25:38 +0100

    c'est pt

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const less = document.getElementsByClassName("quantity-less");
 const stockBtn = document.getElementsByClassName("show-stock");
 const addBtn = document.getElementsByClassName("buy")
 
+
+
 let article = class {
     constructor(type, reference, quantity, price) {
         this.type = type
@@ -17,11 +19,11 @@ let panier = []
 
 for (let i = 0; i < more.length; i++) {
     more[i].addEventListener("click", () => {
-        document.getElementById("quantity-number-"+more[i].dataset.song).value++
+        document.getElementById("quantity-number-"+more[i].dataset.reference).value++
 
     });
     less[i].addEventListener("click", () => {
-        const value = document.getElementById("quantity-number-"+less[i].dataset.song)
+        const value = document.getElementById("quantity-number-"+less[i].dataset.reference)
         if (value.value > 0) {
             value.value--
         }
@@ -29,14 +31,45 @@ for (let i = 0; i < more.length; i++) {
     });
 
     stockBtn[i].addEventListener("click", () => {
-        const stockValue = document.getElementById("stock-number-"+ stockBtn[i].dataset.song)
+        const stockValue = document.getElementById("stock-number-"+ stockBtn[i].dataset.reference)
         stockValue.style.display =  stockValue.style.display == 'block' ? 'none' : 'block' 
     })
 
     addBtn[i].addEventListener("click", () => {
-        let tets = panier.push(new article(addBtn[i].parentNode.className, addBtn[i].dataset.song, document.getElementById("quantity-number-"+addBtn[i].dataset.song).value, 5))
+        let tets = panier.push(new article(addBtn[i].parentNode.className, addBtn[i].dataset.reference, document.getElementById("quantity-number-"+addBtn[i].dataset.reference).value, 5))
     })
    
     
 }
 
+
+function panierLoad() {
+    
+    const table = document.getElementById("panier-article-table")
+    if (panier.length != 0) {
+        panier.forEach(article => {
+            const newRow = document.createElement("tr")
+            const newPreview = document.createElement("th")
+            const newType = document.createElement("th")
+            const newReference = document.createElement("th")
+            const newQuantity = document.createElement("th")
+            const newPrice = document.createElement("th")
+
+            const newPreviewImg = document.createElement("img")
+            newPreviewImg.setAttribute("src", "../img/rickroll.jpg")
+            newPreviewImg.setAttribute("width", "100")
+            newPreviewImg.setAttribute("height", 100)
+            newPreview.append(newPreviewImg)
+
+            newType.textContent(panier.type)
+            newReference.textContent(panier.reference)
+            newQuantity.textContent(panier.quantity)
+            newPrice.textContent(panier.price)
+
+            newRow.append(newPreview, newType, newReference, newQuantity,newPrice)
+            table.append(newRow)
+            console.log("test");
+
+        });
+    }
+}
