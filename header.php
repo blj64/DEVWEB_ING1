3c00e7befb14bdf951516064551fd342b78d5314
jlb64 <jliogerbun+github@gmail.com>
2023-04-07 11:53:32 +0200

    panier page added

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+
     <script src="js/index.js"></script>
     <script src="js/form.js"></script>
     <title>RockHub</title>
@@ -22,11 +23,11 @@
         <div id="connection-div">
                 <?php
                     if (isset($_SESSION["login"]) && isset($_SESSION["mdp"])) {
-                        echo '<button class="connection-btn" id="logout_button" onclick="window.location.href=\'deconnexion.php\'">Log out</button>';
+                        echo '<button class="orange-btn" class="connection-btn" id="logout_button" onclick="window.location.href=\'deconnexion.php\'">Log out</button>';
                         echo '<p id="p-msg-welcome">Bienvenue '.$_SESSION["nom"].'</p>';
                     }else{
-                        echo '<button class="connection-btn" id="login_button" onclick="window.location.href=\'connexion.php\'">Log in</button>';
-                        echo '<button class="connection-btn" id="signup_button" onclick="window.location.href=\'signup.php\'">Sign up</button>';
+                        echo '<button class="orange-btn" class="connection-btn" id="login_button" onclick="window.location.href=\'connexion.php\'">Log in</button>';
+                        echo '<button class="orange-btn" class="connection-btn" id="signup_button" onclick="window.location.href=\'signup.php\'">Sign up</button>';
                     }
                 ?>
             </div>
@@ -35,18 +36,19 @@
                 <div id="search-bar">
                     <input class="input-bar" type="text" placeholder="Search 5436 music" >
                     <div id="search-logo"><img src="./img/loupe.png" alt="O\" height="15" width="20"></div>
-                    <div id="panier-header" >
-                        panier
-                    </div>
+                    <a href="./panier.php">
+
+                        <div class="orange-btn" id="panier-header" >panier</div>
+                </a>
                 </div>
                 
             </div>
             <nav>
-                <a class="nav-link-a" href="index.php"><div class="nav-link">HOME</div></a>
-                <a class="nav-link-a" href="songs.php"><div class="nav-link">SONGS</div></a>
-                <a class="nav-link-a" href="playlist.php"><div class="nav-link">PLAYLIST</div></a>
-                <a class="nav-link-a" href="goodies.php"><div class="nav-link">MERCH</div></a>
-                <a class="nav-link-a" href="contact.php"><div class="nav-link">CONTACT</div></a>
+                <a class="nav-link-a" href="./index.php"><div class="nav-link">HOME</div></a>
+                <a class="nav-link-a" href="./songs.php"><div class="nav-link">SONGS</div></a>
+                <a class="nav-link-a" href="./playlist.php"><div class="nav-link">PLAYLIST</div></a>
+                <a class="nav-link-a" href="./goodies.php"><div class="nav-link">MERCH</div></a>
+                <a class="nav-link-a" href="./contact.php"><div class="nav-link">CONTACT</div></a>
             </nav>
 
             
---
+++ panier.php
@@ -0,0 +1,32 @@
+<?php 
+    include "header.php";
+?>
+        <section id="main-section">
+
+
+            <h1>panier</h1>
+            <div id="panier-body">
+                <div id="panier-article"></div>
+                    <h2>Article</h2>
+                    <table id="panier-article-table">
+                        <tr>
+                            <th>Apercu</th>
+                            <th>Type</th>
+                            <th>Reference</th>
+                            <th>Quantite</th>
+                            <th>Prix</th>
+                        </tr>
+                        
+                    </table>
+
+                    
+                <div id="panier-total">
+                    <h2>Total</h2>
+                    <span>0€</span>
+                </div>
+            </div>
+        </section>
+
+        <?php 
+    include "footer.php";
+?>  
