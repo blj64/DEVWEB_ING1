cccf9c036155b97430ae9a7dbcb3aeb6ea6a1ab5
jlb64 <jliogerbun+github@gmail.com>
2023-04-16 17:48:14 +0200

    signup fix

--- php/signupBack.php
+++ php/signupBack.php
@@ -5,7 +5,7 @@
     $newUser = array($newName, $newLogin, md5($newPassword));
 
     $verif = true;
-    if (($handle = fopen("../csv/users.csv", "a+")) !== FALSE) {
+    if (($handle = fopen("../data/users.csv", "a+")) !== FALSE) {
         while ((($data = fgetcsv($handle, 1000, ";")) !== FALSE) && $verif) {
             if($newLogin == $data[1] || $newName == $data[0]) {
                 $verif = false;
@@ -15,7 +15,7 @@
     }
 
     
-    if ($verif) {
+    if ($verif && ($_POST['password'] != "") && ($_POST['nom'] != "") && ($_POST['login'] != "")) {
         fputcsv($handle, $newUser, ";");
         header("Location: /php/connexion.php");
     }else{
