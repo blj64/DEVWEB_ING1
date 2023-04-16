653517e8c02e67fceb489b5bc8a048dfceac43da
jlb64 <jliogerbun+github@gmail.com>
2023-04-16 19:28:20 +0200

    panier

--- php/panier.php
+++ php/panier.php
@@ -4,18 +4,18 @@ include "header.php";
 <section id="main-section">
 
 
-    <h1>panier</h1>
+    <h1 id="panier-titre">Shopping bag resume</h1>
     <div id="panier-body">
-        <div id="panier-article"></div>
-        <h2>Article</h2>
-        <table id="panier-article-table">
-            <tr>
-                <th>Apercu</th>
-                <th>Reference</th>
-                <th>Quantite</th>
-                <th>P.U.</th>
-            </tr>
-            
+        <div id="panier-article-table">
+            <div id="title-panier">
+                <span class='div-small-img-panier'>Preview</span>
+                <span class="title-panier-description">Reference</span>
+                <span class="title-panier-description">Quantity</span>
+                <span class="title-panier-description">Price</span>
+            </div>
+            <br><br><br>
+            <div id="article-panier">
+         
                 <?php
 
                 $total = 0;
@@ -27,31 +27,35 @@ include "header.php";
                 {
                     foreach ($_SESSION["panier"] as $ligne)
                     {
-                        echo "<tr>";
+                        echo "<div class='product-panier'>";
                         foreach ($ligne as $value)
                         {
                             if ($ligne["apercu"] == $value)
                             {
-                                echo "<th><img class=\"small-img-panier\" src=\"../img/".$value."\" height=\"50px\" width=\"50px\" alt=\"cover\"></th>"; 
+                                echo "<div class='div-small-img-panier'><img class=\"small-img-panier\" src=\"../img/".$value."\" height=\"50px\" width=\"50px\" alt=\"cover\"></div>"; 
                             }
                             else
                             {
-                                echo "<th>".$value."</th>"; 
+                                echo "<div class='panier-article-description'>".$value."</div>"; 
                             }
                             
                         }
                         $total = $total + $ligne["quantite"]*$ligne["prix"];
-                        echo "</tr>";
+                        echo "</div>";
                     }
                 }
                 
                 ?>
+            </div>
 
-        </table>
+            </div>
 
         <div id="panier-total">
-            <h2>Total</h2>
-            <span><?php echo $total ?>€</span>
+            <h1 id="total">Total     <span id="prix-total"><?php echo $total ?>€</span></h1>
+           
+            <h3 id="sous-total">Sous total: <span id="sous-prix-total"><?php echo $total*0.8 ?>€</span></h3>
+            <a  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><div id="payment-link" >PAYMENT</div></a>
+
         </div>
     </div>
 </section>
