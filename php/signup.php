c198c153503f7b72837cf0c14c376cbdc85f7653
jlb64 <jliogerbun+github@gmail.com>
2023-04-16 15:02:06 +0200

    connexion fix(petit bug), signup marche

--- php/signup.php
+++ php/signup.php
@@ -4,43 +4,24 @@ include "header.php";
 
 <section id="main-section">
 
-	<div class="container">
+	<div id="connexion">
+		<h2>Inscription</h2>
 		<?php
 		if ($_GET["error"]) {
-			echo "<div class=error> Identifiant ou mot de passe erroné <br/></div>";
+			echo "<div class=error> Identifiant ou nom non disponible <br/></div>";
 		}
 		?>
 		<!-------------------------------->
-		<form class="login" action="/index.php" method="POST">
-			<div class="login__field">
-				<i class="login__icon fas fa-user"></i>
-				<input type="text" name="login" placeholder="Username" class="login__input">
-			</div>
-			<div class="login__field">
-				<i class="login__icon fas fa-lock"></i>
-				<input type="password" name="password" placeholder="Password" class="login__input">
-			</div>
-			<input class="login__submit" type="submit" value="Connexion">
+		<form class="login" action="/php/signupBack.php" method="POST">
+				<input type="text" name="login" placeholder="Username">
+				<input type="text" name="nom" placeholder="Name">
+				<input type="password" name="password" placeholder="Password" >
+			<input type="submit" value="Inscription">
 
 		</form>
 
 		<!---here----------------------------->
-		<div class="social-login">
-
-
-			<div class="social-icons">
-				<div class="#">
-					<div id="logo-rockhub">
-						<div>Rock</div>
-						<div id="orange">Hub</div>
-					</div>
-				</div>
-
-
-			</div>
-		</div>
-
-
+		
 
 	</div>
 
