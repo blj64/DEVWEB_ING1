3780f9f261b00d9b15d00675b5443610c33a9de6
couziremi <couziremi@cy-tech.fr>
2023-04-16 18:40:53 +0200

    a

--- php/playlist.php
+++ php/playlist.php
@@ -23,91 +23,14 @@
                             <input class=\"quantity-number\" min=\"0\" data-reference=\"".$playlist['id']."\" id=\"quantity-number-".$playlist['id']."\" type=\"number\" value=\"".$playlist['stock']."\" onkeypress=\"return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))\">
                             <button class=\"quantity-more\" data-reference=\"".$playlist['id']."\" id=\"quantity-more-".$playlist['id']."\">+</button>
                         </div>
-                        <button class=\"buy\" data-reference=\"".$playlist['id']."\" id=\"buy-".$playlist['id']."\">+</button>
+
+                        <button class=\"buy\" data-reference=\"".$playlist['id']."\" id=\"buy-".$playlist['id']."\" onclick=\"ajouterQuantitePanier('".$playlist['id']."')\">Ajouter au panier</button>
                     </div>";
                 }
 
                 
                 ?>
-                <!-- div class="playlist" data-reference="100%DesertRock-Stoner" id="playlist-100%DesertRock-Stoner">
-                    <img class="playlist-img" data-reference="100%DesertRock-Stoner" id="playlist-img-100%DesertRock-Stoner" src="../img/desertRock.jpeg" height="200px" width="200px" alt="">
-                    <h2 class="playlist-name" data-reference="100%DesertRock-Stoner" id="playlist-name-100%DesertRock-Stoner">100% Desert Rock</h2>
-                    <h3 class="playlist-artist" data-reference="100%DesertRock-Stoner" id="playlist-artist-100%DesertRock-Stoner">Stoner</h3>
-                    <div class="stock" data-reference="100%DesertRock-Stoner" id="stock-100%DesertRock-Stoner">
-                        <button class="show-stock" data-reference="100%DesertRock-Stoner" id="show-stock-100%DesertRock-Stoner">stock</button>
-                        <div  class="stock-number" data-reference="100%DesertRock-Stoner" id="stock-number-100%DesertRock-Stoner">5</div>
-                    </div>
-                    <div class="quantity" data-reference="100%DesertRock-Stoner" id="quantity-100%DesertRock-Stoner">
-                        <button class="quantity-less" data-reference="100%DesertRock-Stoner" id="quantity-less-100%DesertRock-Stoner">-</button>
-                        <input class="quantity-number" min="0" data-reference="100%DesertRock-Stoner" id="quantity-number-100%DesertRock-Stoner" type="number" value="0" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))">
-                        <button class="quantity-more" data-reference="100%DesertRock-Stoner" id="quantity-more-100%DesertRock-Stoner">+</button>
-                    </div>
-                    <button class="buy" data-reference="100%DesertRock-Stoner" id="buy-100%DesertRock-Stoner">+</button>
-                </div>
-
-                <div class="playlist" data-reference="100%RockFrancais-Rock" id="playlist-100%RockFrancais-Rock">
-                    <img class="playlist-img" data-reference="100%RockFrancais-Rock" id="playlist-img-100%RockFrancais-Rock" src="../img/rockfr.jpeg" height="200px" width="200px" alt="">
-                    <h2 class="playlist-name" data-reference="100%RockFrancais-Rock" id="playlist-name-100%RockFrancais-Rock">100% Rock Français</h2>
-                    <h3 class="playlist-artist" data-reference="100%RockFrancais-Rock" id="playlist-artist-100%RockFrancais-Rock">Rock</h3>
-                    <div class="stock" data-reference="100%RockFrancais-Rock" id="stock-100%RockFrancais-Rock">
-                        <button class="show-stock" data-reference="100%RockFrancais-Rock" id="show-stock-100%RockFrancais-Rock">stock</button>
-                        <div  class="stock-number" data-reference="100%RockFrancais-Rock" id="stock-number-100%RockFrancais-Rock">3</div>
-                    </div>
-                    <div class="quantity" data-reference="100%RockFrancais-Rock" id="quantity-100%RockFrancais-Rock">
-                        <button class="quantity-less" data-reference="100%RockFrancais-Rock" id="quantity-less-100%RockFrancais-Rock">-</button>
-                        <input class="quantity-number" min="0" data-reference="100%RockFrancais-Rock" id="quantity-number-100%RockFrancais-Rock" type="number" value="0" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))">
-                        <button class="quantity-more" data-reference="100%RockFrancais-Rock" id="quantity-more-100%RockFrancais-Rock">+</button>
-                    </div>
-                    <button class="buy" data-reference="100%RockFrancais-Rock" id="buy-100%RockFrancais-Rock">+</button>
-                </div>
-
-                <div class="playlist" data-reference="100%Acoustic-Solo" id="playlist-100%Acoustic-Solo">
-                    <img class="playlist-img" data-reference="100%Acoustic-Solo" id="playlist-img-100%Acoustic-Solo" src="../img/acoustic.jpeg" height="200px" width="200px" alt="">
-                    <h2 class="playlist-name" data-reference="100%Acoustic-Solo" id="playlist-name-100%Acoustic-Solo">100% Acoustic</h2>
-                    <h3 class="playlist-artist" data-reference="100%Acoustic-Solo" id="playlist-artist-100%Acoustic-Solo">Solo</h3>
-                    <div class="stock" data-reference="100%Acoustic-Solo" id="Solo-100%Acoustic-Solo">
-                        <button class="show-stock" data-reference="100%Acoustic-Solo" id="show-stock-100%Acoustic-Solo">stock</button>
-                        <div  class="stock-number" data-reference="100%Acoustic-Solo" id="stock-number-100%Acoustic-Solo">4</div>
-                    </div>
-                    <div class="quantity" data-reference="100%Acoustic-Solo" id="quantity-100%Acoustic-Solo">
-                        <button class="quantity-less" data-reference="100%Acoustic-Solo" id="quantity-less-100%Acoustic-Solo">-</button>
-                        <input class="quantity-number" min="0" data-reference="100%Acoustic-Solo" id="quantity-number-100%Acoustic-Solo" type="number" value="0" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))">
-                        <button class="quantity-more" data-reference="100%Acoustic-Solo" id="quantity-more-100%Acoustic-Solo">+</button>
-                    </div>
-                    <button class="buy" data-reference="100%Acoustic-Solo" id="buy-100%Acoustic-Solo">+</button>
-                </div>
-
-                <div class="playlist" data-reference="100%MetalIndus-Metal" id="playlist-100%MetalIndus-Metal">
-                    <img class="playlist-img" data-reference="100%MetalIndus-Metal" id="playlist-img-100%MetalIndus-Metal" src="../img/metalindus.jpeg" height="200px" width="200px" alt="">
-                    <h2 class="playlist-name" data-reference="100%MetalIndus-Metal" id="playlist-name-100%MetalIndus-Metal">100% Métal Industriel</h2>
-                    <h3 class="playlist-artist" data-reference="100%MetalIndus-Metal" id="playlist-artist-100%MetalIndus-Metal">Métal</h3>
-                    <div class="stock" data-reference="100%MetalIndus-Metal" id="stock-100%MetalIndus-Metal">
-                        <button class="show-stock" data-reference="100%MetalIndus-Metal" id="show-stock-100%MetalIndus-Metal">stock</button>
-                        <div  class="stock-number" data-reference="100%MetalIndus-Metal" id="stock-number-100%MetalIndus-Metal">70</div>
-                    </div>
-                    <div class="quantity" data-reference="100%MetalIndus-Metal" id="quantity-100%MetalIndus-Metal">
-                        <button class="quantity-less" data-reference="100%MetalIndus-Metal" id="quantity-less-100%MetalIndus-Metal">-</button>
-                        <input class="quantity-number" min="0" data-reference="100%MetalIndus-Metal" id="quantity-number-100%MetalIndus-Metal" type="number" value="0" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))">
-                        <button class="quantity-more" data-reference="100%MetalIndus-Metal" id="quantity-more-100%MetalIndus-Metal">+</button>
-                    </div>
-                    <button class="buy" data-reference="100%MetalIndus-Metal" id="buy-100%MetalIndus-Metal">+</button>
-                </div>
-
-                <div class="playlist" data-reference="100%RockCalifornien-Rock" id="playlist-100%RockCalifornien-Rock">
-                    <img class="playlist-img" data-reference="100%RockCalifornien-Rock" id="playlist-img-100%RockCalifornien-Rock" src="../img/californianrock.jpeg" height="200px" width="200px" alt="">
-                    <h2 class="playlist-name" data-reference="100%RockCalifornien-Rock" id="playlist-name-100%RockCalifornien-Rock">100% Rock Californien</h2>
-                    <h3 class="playlist-artist" data-reference="100%RockCalifornien-Rock" id="playlist-artist-100%RockCalifornien-Rock">Rock</h3>
-                    <div class="stock" data-reference="100%RockCalifornien-Rock" id="stock-100%RockCalifornien-Rock">
-                        <button class="show-stock" data-reference="100%RockCalifornien-Rock" id="show-stock-100%RockCalifornien-Rock">stock</button>
-                        <div  class="stock-number" data-reference="100%RockCalifornien-Rock" id="stock-number-100%RockCalifornien-Rock">10</div>
-                    </div>
-                    <div class="quantity" data-reference="100%RockCalifornien-Rock" id="quantity-100%RockCalifornien-Rock">
-                        <button class="quantity-less" data-reference="100%RockCalifornien-Rock" id="quantity-less-100%RockCalifornien-Rock">-</button>
-                        <input class="quantity-number" min="0" data-reference="100%RockCalifornien-Rock" id="quantity-number-100%RockCalifornien-Rock" type="number" value="0" onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))">
-                        <button class="quantity-more" data-reference="100%RockCalifornien-Rock" id="quantity-more-100%RockCalifornien-Rock">+</button>
-                    </div>
-                    <button class="buy" data-reference="100%RockCalifornien-Rock" id="buy-100%RockCalifornien-Rock">+</button>
-                </div-->
+                
 
                 
             </div>
--- php/songs.php
+++ php/songs.php
@@ -24,15 +24,13 @@
                 <img class=\"song-img\" data-reference=\"".$song['id']."\" id=\"song-img-".$song['id']."\" src=\"../img/".$song['cover']."\" height=\"200px\" width=\"200px\" alt=\"\">
                 <h2 class=\"song-name\" data-reference=\"".$song['id']."\" id=\"song-name-".$song['id']."\">".$song['name']."</h2>
                 <h3 class=\"song-artist\" data-reference=\"".$song['id']."\" id=\"song-artist-".$song['id']."\">".$song['complement']."</h3>
-                <!-- div class=\"stock\" data-reference=\"".$song['id']."\" id=\"song-stock-".$song['id']."\">
-                    <button class=\"show-stock\" data-reference=\"".$song['id']."\" id=\"show-stock-".$song['id']."\">stock</button>
-                    <div class=\"stock-number\" data-reference=\"".$song['id']."\" id=\"stock-number-".$song['id']."\">10</div>
-                </div-->
+
                 <div class=\"quantity\" data-reference=\"".$song['id']."\" id=\"quantity-".$song['id']."\">
                     <button data-reference=\"".$song['id']."\"class=\"quantity-less\" id=\"quantity-less-".$song['id']."\" onclick=\"decrementer('".$song['id']."')\">-</button>
                     <span class=\"quantity-number\" min=\"0\" max=\"".$song['stock']."\" data-reference=\"".$song['id']."\" id=\"quantity-number-".$song['id']."\" type=\"number\">".$song['panier']."</span>
                     <button data-reference=\"".$song['id']."\" class=\"quantity-more\" id=\"quantity-more-".$song['id']."\" onclick=\"incrementer('".$song['id']."','".$song['stock']."')\">+</button>
                 </div>
+                
                 <button class=\"buy\" data-reference=\"".$song['id']."\" id=\"buy-".$song['id']."\" onclick=\"ajouterQuantitePanier('".$song['id']."')\">Ajouter au panier</button>
             </div>";
         }
